7e2e2581ee065fabb242f4cfd14362304a413fb8
anaitezk <anaitezk@hotmail.gr>
2023-11-02 21:37:49 +0200

    size() to get_size()

--- project1/src/graph.cpp
+++ project1/src/graph.cpp
@@ -46,13 +46,13 @@ void Graph::display_graph()
 
 double Graph::euclideanDistance(const Vector<T> &point1, const Vector<T> &point2)
 {
-    if (point1.size() != point2.size())
+    if (point1.get_size() != point2.get_size())
     {
         throw std::invalid_argument("Points should have the same dimensions.");
     }
 
     double distance = 0.0;
-    for (size_t i = 0; i < point1.size(); i++)
+    for (size_t i = 0; i < point1.get_size(); i++)
     {
         distance += pow(point1[i] - point2[i], 2);
     }
@@ -61,13 +61,13 @@ double Graph::euclideanDistance(const Vector<T> &point1, const Vector<T> &point2
 
 double Graph::manhattanDistance(const Vector<T> &point1, const Vector<T> &point2)
 { 
-    if (point1.size()!= point2.size())
+    if (point1.get_size()!= point2.get_size())
     {
         throw std::invalid_argument("Points should have the same dimensions.");
     }
 
     double distance = 0.0;
-    for (size_t i = 0; i < point1.size(); i++)
+    for (size_t i = 0; i < point1.get_size(); i++)
     {
         distance += abs(point1[i] - point2[i]);
     }
