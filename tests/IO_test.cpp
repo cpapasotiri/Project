c96f7df7afa72c6b475eef4eb0fe623e9daccb38
cpapasotiri <nina.papasotiri>
2023-11-17 13:47:19 +0200

    use distance type as a parameter

--- tests/IO_test.cpp
+++ tests/IO_test.cpp
@@ -6,28 +6,32 @@ TEST_CASE("IO functionality tests", "[IO]"){
     SECTION("Testing create output filepath using datasets/2dims.bin inputFile")
     {
         char outputPath[len];
-        create_output_filepath("datasets/2dims.bin", "e", outputPath, sizeof(outputPath));
+        char distance[2] = "e";
+        create_output_filepath("datasets/2dims.bin", distance, outputPath, sizeof(outputPath));
         REQUIRE(strcmp(outputPath, "output/2dims_e.bin") == 0);
     }
 
     SECTION("Testing create output filepath using datasets/00000020.bin inputFile")
     {
         char outputPath[len];
-        create_output_filepath("datasets/00000020.bin", "m", outputPath, sizeof(outputPath));
+        char distance[2] = "m";
+        create_output_filepath("datasets/00000020.bin", distance, outputPath, sizeof(outputPath));
         REQUIRE(strcmp(outputPath, "output/00000020_m.bin") == 0);
     }
 
     SECTION("Testing create output filepath using 2dims.bin inputFile")
     {
         char outputPath[len];
-        create_output_filepath("2dims.bin", "m", outputPath, sizeof(outputPath));
+        char distance[2] = "m";
+        create_output_filepath("2dims.bin", distance, outputPath, sizeof(outputPath));
         REQUIRE(strcmp(outputPath, "output/2dims_m.bin") == 0);
     }
 
     SECTION("Testing create output filepath using include/2dims.bin inputFile")
     {
         char outputPath[len];
-        create_output_filepath("include/2dims.bin", "e", outputPath, sizeof(outputPath));
+        char distance[2] = "e";
+        create_output_filepath("include/2dims.bin", distance, outputPath, sizeof(outputPath));
         REQUIRE(strcmp(outputPath, "output/2dims_e.bin") == 0);
     }
 }
